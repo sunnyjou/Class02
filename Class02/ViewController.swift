c89126ba04017f9519ed716a82029c3ef92f3f4e
sunnyjou <sunny_chou@mail.cjcu.edu.tw>
2019-04-15 11:24:11 +0800

    123

--- Class02/ViewController.swift
+++ Class02/ViewController.swift
@@ -12,10 +12,51 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.   
+        // Do any additional setup after loading the view, typically from a nib.
 
+        /*
+        let i : Int = 1
+
+        if (i > 0) {
+            print("> 0")
+        } else {
+            print("<= 0")
+        }
+
+         
+        
+        let val : Int = 2
+        switch val {
+            case 1:
+                print("1")
+                break
+            case 2:
+                print("2")
+                break
+            default:
+                print("***")
+                break
+        }
+        
+         
+         
+        let arr = [1,11,111,1111]
+        for a in arr {
+         print(a)
+        }
+         
+        */
+        
+
+
+        
+        
     }
     
+    
+    func Add(first:Int, second:Int) -> Int {
+        return first+second
+    }
 
     
 }
